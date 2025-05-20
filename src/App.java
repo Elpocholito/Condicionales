308221f785558af452fd287e29e6c257ab14e1f4
unknown <emachavarria5@ielasnieves.edu.co>
2025-05-20 15:38:30 -0500

    Condicionales

---
+++ src/App.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+         try (Scanner Scanner = new Scanner (System.in)) {
+            int n= Scanner.nextInt(); //Lea n
+            int m= Scanner.nextInt(); //Lea m
+
+            if (n%2==0 && m%2==0){ 
+               System.out.println("n y m son pares") ; //Muestre "n y m son pares"
+            } else if (n%2!=0 && m%2==0){ 
+                System.out.println("solo m es par") ; 
+            }else if (n%2==0 && m%2!=0){
+                System.out.println("solo n es par") ; 
+            } else {
+                System.out.println("n y m son impares") ; 
+            }
+         }
+
+    }
+}
